0a2243d4bd85e58d2fe144eeef49bcf69424a613
lijie <jetlee1906@163.com>
2018-08-31 19:53:28 +0800

    add:用户注册验证邮箱是否已被注册

--- app/form/user.py
+++ app/form/user.py
@@ -1,5 +1,7 @@
 from wtforms import Form, StringField, PasswordField
-from wtforms.validators import DataRequired, Email, Length, EqualTo
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError, StopValidation
+
+from app.model.user import User
 
 
 class UserLoginForm(Form):
@@ -12,3 +14,9 @@ class UserRegisterForm(Form):
     nickname = StringField(validators=[DataRequired('请输入昵称'), Length(1, 20, '昵称长度不符合规范')])
     password = PasswordField(validators=[DataRequired('请输入密码'), Length(6, 30, '密码长度不符合规范')])
     confirm = PasswordField(validators=[DataRequired('请确认密码'), EqualTo('password', '两次输入的密码不一致')])
+
+    def validate_email(self, field):
+        user = User.query.filter_by(email=field.data).first()
+        if user:
+            # raise ValidationError(message='该邮箱已被注册')
+            raise StopValidation(message='该邮箱已被注册')
